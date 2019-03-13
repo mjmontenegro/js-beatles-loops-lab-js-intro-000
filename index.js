3f441b591b7c17ca7fc8899766a007d0566f6416
Mauricio Javier Montenegro <mjmontenegro@gmail.com>
2019-03-13 05:27:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let result = [];
+  for(let i = 0; i < musicians.length; i++) {
+    result.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return result;
+}
+function johnLennonFacts(facts) {
+  let i = 0;
+  while(i < facts.length) {
+    facts[i] = facts[i] + "!!!";
+    i++;
+  }
+  return facts;
+}
+function iLoveTheBeatles(number) {
+  let result = [];
+  do {
+    result.push("I love the Beatles!");
+    number++;
+  } while (number < 15);
+  return result;
+}
